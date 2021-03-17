47a768e03ddf250adb344702501a20ca7e6c96fd
mapogolions <ivanovpavelalex45@gmail.com>
2021-03-17 23:48:16 +0500

    add some trivial tests

--- Koa.Middleware.Tests/MiddlewareStackTests.cs
+++ Koa.Middleware.Tests/MiddlewareStackTests.cs
@@ -1,13 +1,67 @@
+using System;
 namespace Koa.Middleware.Tests
 {
     using Xunit;
 
     public class MiddlewareStackTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("foo", "FOO")]
+        [InlineData("B-a-r", "B_A_R")]
+        public void ShoulBeAbleCallNext(string input, string expected)
         {
-            Assert.True(true);
+            var stack = new MiddlewareStack<string>();
+            var fn = stack.Compose((_, next) =>
+            {
+                return next().ToUpper();
+            },
+            (_, next) =>
+            {
+                return _.Replace('-', '_');;
+            }
+            );
+
+            var actual = fn(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void ShouldIncrementInputValue(int input)
+        {
+            var stack = new MiddlewareStack<int>();
+            var fn = stack.Compose((_, next) =>
+            {
+                return _ + 1;
+            });
+            var actual = fn(input);
+            var expected = input + 1;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenMiddlewareIsNull()
+        {
+            var stack = new MiddlewareStack<string>();
+
+            Assert.Throws<ArgumentNullException>(() => stack.Compose(null));
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("foo")]
+        public void ShouldBeLikeIdentityFunctionWhenMiddlewareIsEmptyCollection(string expected)
+        {
+            var stack = new MiddlewareStack<string>();
+            var fn = stack.Compose(Array.Empty<Func<string, Func<string>, string>>());
+            var actual = fn(expected);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
